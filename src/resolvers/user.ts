f964ca4552ba76cf85a299ac1824c3f788297353
Daniel Amoils <danieljamoils@gmail.com>
2020-08-24 20:52:39 -0400

    sessions authentication

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -5,7 +5,8 @@ import {
     InputType,
     Field,
     Ctx,
-    ObjectType
+    ObjectType,
+    Query
 } from "type-graphql"
 import { MyContext } from "src/types"
 import { User } from "../entities/User"
@@ -38,10 +39,21 @@ class UserResponse {
 
 @Resolver()
 export class UserResolver {
+    @Query(() => User, { nullable: true })
+    async me(@Ctx() { req, em }: MyContext) {
+        //you are not logged in
+        if (!req.session.userId) {
+            return null
+        }
+
+        const user = await em.findOne(User, { id: req.session.userId })
+        return user
+    }
+
     @Mutation(() => UserResponse)
     async register(
         @Arg("options") options: UsernamePasswordInput,
-        @Ctx() { em }: MyContext
+        @Ctx() { em, req }: MyContext
     ): Promise<UserResponse> {
         if (options.username.length <= 2) {
             return {
@@ -87,13 +99,15 @@ export class UserResolver {
             }
             console.log("Message:", err.message)
         }
+        req.session!.userId = user.id
+
         return { user }
     }
 
     @Mutation(() => UserResponse)
     async login(
         @Arg("options") options: UsernamePasswordInput,
-        @Ctx() { em }: MyContext
+        @Ctx() { em, req }: MyContext
     ): Promise<UserResponse> {
         const user = await em.findOne(User, { username: options.username })
         if (!user) {
@@ -117,6 +131,9 @@ export class UserResolver {
                 ]
             }
         }
+
+        req.session!.userId = user.id
+
         return {
             user
         }
